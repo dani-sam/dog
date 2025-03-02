ae9d2f3ff6463aae86e85c133aa5e8af8d5faddd
dani-sam <samdani295@gmail.com>
2025-03-03 00:28:21 +0530

    second

---
+++ dog.go
@@ -0,0 +1,8 @@
+package dog
+
+import "strings"
+
+
+func WhenGrownUp(s string) string {
+	return "when the puppy grows up it say: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dani-sam/dog
+
+go 1.24.0
